7d27daa06d49f196a86fdd81496f609b17620d85
Michele Scuttari <michele.scuttari@outlook.com>
2023-02-07 22:04:31 +0100

    Fix indentation

--- submission.py
+++ submission.py
@@ -1,6 +1,6 @@
 class Submission:
-        def __init__(self, id, time, source_code, result):
-                self.id = id
-                self.time = time
-                self.source_code = source_code
-                self.result = result
+	def __init__(self, id, time, source_code, result):
+		self.id = id
+		self.time = time
+		self.source_code = source_code
+		self.result = result
--- test_case.py
+++ test_case.py
@@ -1,6 +1,6 @@
 class TestCase:
-        def __init__(self, id, number, description, result):
-                self.id = id
-                self.number = number
-                self.description = description
-                self.result = result
+	def __init__(self, id, number, description, result):
+		self.id = id
+		self.number = number
+		self.description = description
+		self.result = result
